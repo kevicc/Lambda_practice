eef11cab5f8baaba3d795a453136d4002e435e5b
Kelsey Pericak <kelspericak@gmail.com>
2022-07-01 18:42:25 -0400

    added practice code

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#https://www.youtube.com/watch?v=AmHZxULclLQ
+
+def lambda_function(event, context):
+    d = {'col1':[w,2],'col2':[3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1'
+    )
+    
